342422e6c91ee6c27afff8364447e31482a1ec0e
Mambo <kdevkr@gmail.com>
2023-11-26 10:19:45 +0900

    workflow: checkstyle.yml

--- backend/src/main/java/kr/kdev/app/IndexController.java
+++ backend/src/main/java/kr/kdev/app/IndexController.java
@@ -1,14 +1,12 @@
 package kr.kdev.app;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Index_Controller {
+public class IndexController {
     @GetMapping("/")
     public String index() {
-//        System.out.println("sss");
         return "ok";
     }
 }
